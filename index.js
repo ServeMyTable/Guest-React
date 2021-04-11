615e5069bcd4ddc01f7987e9d52327cc688b45c4
ServeMyTable <66901543+ServeMyTable@users.noreply.github.com>
2021-04-11 21:19:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 const app = express();
 
 //Connect to Database
@@ -7,7 +8,7 @@ connectDB();
 
 //Initialize Middleware
 app.use(express.json());
-//app.use(express.static(path.join(__dirname, "/frontend/build")));
+app.use(express.static(path.join(__dirname, "/frontend/build")));
 
 app.use('/api/auth',require('./routes/api/auth'));
 app.use('/api/restaurant',require('./routes/api/restaurant'));
@@ -16,7 +17,7 @@ app.use('/api/feedback',require('./routes/api/feedback'));
 app.use('/api/token',require('./routes/api/token'));
 app.use('/api/parcel',require('./routes/api/parcel'));
 
-//app.get("/*", (req, res) => { res.sendFile(path.join(__dirname, "/frontend/build/index.html")); });
+app.get("/*", (req, res) => { res.sendFile(path.join(__dirname, "/frontend/build/index.html")); });
 
 app.listen(process.env.PORT || 5000,function(){ 
     console.log('Server is up and Running on http://localhost:5000'); 
